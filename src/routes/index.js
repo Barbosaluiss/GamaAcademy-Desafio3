76e3ccd8f37358cf0430dc76cbd76a204c286613
Giovana <85407326+resetgirl@users.noreply.github.com>
2022-12-16 17:06:26 -0300

    Atualizado com CRUD Psicólogos correto + login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,21 @@
 const express = require("express");
 const psicologosController = require("../controllers/psicologosController");
 
-// const auth = require("../middlewares/auth");
+const authController = require("../controllers/authController");
+const requestLog = require("../middlewares/requestLog");
+
+const authLoginValidation = require("../validations/auth/login");
+
+const auth = require("../middlewares/auth");
 const routes = express.Router();
 
 // ----->>>>> CRUD
-routes.get("/psicologos", psicologosController.listarPsicologos);
+routes.get("/psicologos", requestLog, psicologosController.listarPsicologos);
 routes.get("/psicologos/:id", psicologosController.listarPsicologobyId);
 routes.post("/psicologos", psicologosController.cadastrarPsicologo);
 routes.delete("/psicologos/:id", psicologosController.deletarPsicologo);
 routes.put("/psicologos/:id", psicologosController.atualizarCadastro);
+
+routes.post("/login", authLoginValidation, authController.login);
 
 module.exports = routes;
